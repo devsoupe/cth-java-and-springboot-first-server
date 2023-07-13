cf83366501c63c907b36244451e4643c960f141d
devsoupe <luvyum4e@gmail.com>
2023-07-14 00:47:36 +0900

    6강. POST API 개발하고 테스트하기

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,9 +1,8 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CalculatorController {
@@ -12,8 +11,8 @@ public class CalculatorController {
      * http://localhost:8080/addv2?number1=10&number2=20
      */
 
-    @GetMapping("/add") // GET /add?number1=10&number2=20
-    public int addTwoNumbersV1(
+    @GetMapping("/add") // GET /add
+    public int addTwoNumbersV1( // ?number1=10&number2=20
             @RequestParam int number1,
             @RequestParam int number2
     ) {
@@ -23,6 +22,11 @@ public class CalculatorController {
     @GetMapping("/addv2")
     public int addTwoNumbersV2(CalculatorAddRequest request) {
         return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply") // POST /multiply
+    public int multiplyNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNumber1() * request.getNumber2();
     }
 
 }
